fac7b234c7351a79105cc6eaa998a15f3c5e6ffa
Tomohito Higuchi <tomgoodsun+github@gmail.com>
2021-11-27 03:26:32 +0900

    Update script

--- apache_tools/apache_create_vhost.sh
+++ apache_tools/apache_create_vhost.sh
@@ -13,7 +13,7 @@ APACHE_VHOST_NAME=""
 APACHE_LOG_DIR="/var/log/httpd"
 APACHE_ACCESS_LOG_NAME="access_log"
 APACHE_ERROR_LOG_NAME="error_log"
-APACHE_VHOST_CONF_DIR=$(${APACHE_CTL} -S | grep 'ServerRoot: ' | awk 'BEGIN{FS="\""}{print $2}')
+APACHE_VHOST_CONF_DIR=$(${APACHE_CTL} -S | grep 'ServerRoot: ' | awk 'BEGIN{FS="\""}{print $2}')"/conf.d"
 APACHE_VHOST_CONF_PREFIX="vhost_"
 APACHE_VHOST_ROOT_DIR="/var/www/vhosts"
 APACHE_VHOST_DOC_ROOT_NAME="html"
@@ -46,7 +46,7 @@ EOS
 
 get_next_vhost_no() {
     number=$(
-        find /etc/httpd/conf.d/ -type f -name ${APACHE_VHOST_CONF_PREFIX}'*.conf' \
+        find ${APACHE_VHOST_CONF_DIR}/ -type f -name ${APACHE_VHOST_CONF_PREFIX}'*.conf' \
             | awk --assign=prefix=${APACHE_VHOST_CONF_PREFIX} 'BEGIN{FS=prefix}{print $(NF)}' \
                 | awk 'BEGIN{FS="-"}{print $1}' \
                     | sort -r \
@@ -77,40 +77,51 @@ OPTS=`getopt -o hug: --long admin,user,group,log-dir,access-log-name,error-log-n
 
 if [ $? != 0 ] ;
 then
-    echo "Failed parsing options." >&2 ;
-    exit 1 ;
+    echo "Failed parsing options." >&2
+    exit 1
 fi
 
-while true; do
-    case "$1" in
+while true;
+do
+    case "${1}" in
         -h | --help ) help; exit 0 ;;
-        -u | --user )       APACHE_USER="$2";                    shift; shift ;;
-        -g | --group )      APACHE_GROUP="$2";                   shift; shift ;;
-        -p | --port )       APACHE_VHOST_PORT="$2";              shift; shift ;;
-        --admin )           APACHE_SERVER_ADMIN="$2";            shift; shift ;;
-        --use-ssl )         APACHE_USE_SSL=true;                 shift ;;
-        --ssl-port )        APACHE_VHOST_SSL_PORT="$2";          shift; shift ;;
-        --ssl-cert )        APACHE_VHOST_SSL_CERT_FILE="$2";     shift; shift ;;
-        --ssl-cert-key )    APACHE_VHOST_SSL_CERT_KEY_FILE="$2"; shift; shift ;;
-        --log-dir )         APACHE_LOG_DIR="$2";                 shift; shift ;;
-        --access-log-name ) APACHE_ACCESS_LOG_NAME="$2";         shift; shift ;;
-        --error-log-name )  APACHE_ERROR_LOG_NAME="$2";          shift; shift ;;
-        --root-dir )        APACHE_VHOST_ROOT_DIR="$2";          shift; shift ;;
-        --doc-root-name )   APACHE_VHOST_DOC_ROOT_NAME="$2";     shift; shift ;;
+        -u | --user )       APACHE_USER="${2}";                    shift; shift ;;
+        -g | --group )      APACHE_GROUP="${2}";                   shift; shift ;;
+        -p | --port )       APACHE_VHOST_PORT="${2}";              shift; shift ;;
+        --admin )           APACHE_SERVER_ADMIN="${2}";            shift; shift ;;
+        --use-ssl )         APACHE_USE_SSL=true;                   shift ;;
+        --ssl-port )        APACHE_VHOST_SSL_PORT="${2}";          shift; shift ;;
+        --ssl-cert )        APACHE_VHOST_SSL_CERT_FILE="${2}";     shift; shift ;;
+        --ssl-cert-key )    APACHE_VHOST_SSL_CERT_KEY_FILE="${2}"; shift; shift ;;
+        --log-dir )         APACHE_LOG_DIR="${2}";                 shift; shift ;;
+        --access-log-name ) APACHE_ACCESS_LOG_NAME="${2}";         shift; shift ;;
+        --error-log-name )  APACHE_ERROR_LOG_NAME="${2}";          shift; shift ;;
+        --root-dir )        APACHE_VHOST_ROOT_DIR="${2}";          shift; shift ;;
+        --doc-root-name )   APACHE_VHOST_DOC_ROOT_NAME="${2}";     shift; shift ;;
         -- ) shift; break ;;
         * ) APACHE_VHOST_NAME=${1}; shift; break ;;
     esac
 done
 
-#echo $APACHE_USER
-#echo $APACHE_GROUP
-#echo $APACHE_VHOST_NAME
-#echo $APACHE_LOG_DIR
-#echo $APACHE_ACCESS_LOG_NAME
-#echo $APACHE_ERROR_LOG_NAME
-#echo $APACHE_VHOST_CONF_DIR
-#echo $APACHE_VHOST_ROOT_DIR
-#echo $APACHE_VHOST_DOC_ROOT_NAME
+# Testing variables
+#echo '01: ' $APACHE_USER
+#echo '02: ' $APACHE_GROUP
+#echo '03: ' $APACHE_VHOST_NAME
+#echo '04: ' $APACHE_LOG_DIR
+#echo '05: ' $APACHE_ACCESS_LOG_NAME
+#echo '06: ' $APACHE_ERROR_LOG_NAME
+#echo '07: ' $APACHE_VHOST_CONF_DIR
+#echo '08: ' $APACHE_VHOST_ROOT_DIR
+#echo '09: ' $APACHE_VHOST_DOC_ROOT_NAME
+#echo '10: ' $APACHE_USE_SSL
+#vhost_conf=${APACHE_VHOST_CONF_DIR}/${APACHE_VHOST_CONF_PREFIX}$(get_next_vhost_no)-${APACHE_VHOST_NAME}.conf
+#vhost_dir=${APACHE_VHOST_ROOT_DIR}/${APACHE_VHOST_NAME}
+#vhost_doc_root=${vhost_dir}/${APACHE_VHOST_DOC_ROOT_NAME}
+#apache_log_dir=${vhost_dir}/httpd
+#echo '11: ' $vhost_conf
+#echo '12: ' $vhost_dir
+#echo '13: ' $vhost_doc_root
+#echo '14: ' $apache_log_dir
 #exit
 
 if [ "" = "${APACHE_VHOST_NAME}" ];
@@ -133,12 +144,23 @@ then
 fi
 
 # Check if config file already exists or not.
-vhost_conf=${APACHE_VHOST_CONF_DIR}/${APACHE_VHOST_NAME}.conf
-if [ -e ${vhost_conf} ];
+vhost_conf=${APACHE_VHOST_CONF_DIR}/${APACHE_VHOST_CONF_PREFIX}$(get_next_vhost_no)-${APACHE_VHOST_NAME}.conf
+same_vhost_count=$(grep "${APACHE_VHOST_NAME}" ${APACHE_VHOST_CONF_DIR}/${APACHE_VHOST_CONF_PREFIX}*.conf | wc -l)
+if [ ${same_vhost_count} != 0 ];
 then
-    echo "Virtual host conf '${vhost_conf}' already exists."
+    echo "Virtual host conf for '${APACHE_VHOST_NAME}' already exists."
     help
     exit 1
+fi
+
+# Check SSL arguments requirement
+if [ "${APACHE_USE_SSL}" = "true" ];
+then
+    if [ "${APACHE_VHOST_SSL_CERT_FILE}" = "" ] || [ "${APACHE_VHOST_SSL_CERT_KEY_FILE}" = "" ];
+    then
+        echo "--use-ssl requires arguments --ssl-cert and  --ssl-cert-key."
+        exit 1
+    fi
 fi
 
 # Check if virtual host directory already exists or not.
@@ -192,7 +214,7 @@ cat << EOS > ${vhost_conf}
 
 EOS
 
-if [ ${APACHE_USE_SSL} ];
+if [ "${APACHE_USE_SSL}" = "true" ];
 then
     # Generates virtual host config.
     cat << EOS >> ${vhost_conf}
